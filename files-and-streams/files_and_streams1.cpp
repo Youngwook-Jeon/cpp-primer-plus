63d367589cdcf013a29303d969396f7c106e084c
Young wook Jeon <ela87@naver.com>
2021-08-16 20:09:56 +0900

    commit

---
+++ files-and-streams/files_and_streams1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream> // for ifstream and ofstream
+using namespace std;
+
+int main() {
+    ifstream fin("read_file.txt");
+
+    if (fin.fail()) {
+        cout << "Can't open the file\n";
+        return 0;
+    }
+    int x, y;
+    fin >> x >> y;
+    cout << x + y;
+    fin.close();
+
+    ofstream fout("my_output.txt");
+    if (fout.fail()) {
+        cout << "Can't open the output file\n";
+        return 0;
+    }
+    fout << x + y;
+    fout.close();
+    return 0;
+}
